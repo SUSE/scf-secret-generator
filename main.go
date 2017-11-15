81121f17115e9f5d72818aee76962dfef9ab0290
Alex Harford <alex.harford@suse.com>
2017-11-15 11:04:57 -0800

    Fill in the password generation function

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	//"time"
+	"os"
+	"strings"
 
 	"github.com/dchest/uniuri"
 	"gopkg.in/yaml.v2"
-	//"k8s.io/apimachinery/pkg/api/errors"
-	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	//"k8s.io/client-go/kubernetes"
-	//"k8s.io/client-go/rest"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
+
+// The name of the secret stored in the kube API
+const SECRET_NAME = "secret"
 
 // GeneratorType describes the type of generator used for the configuration value
 type GeneratorType string
@@ -82,16 +87,32 @@ type Manifest struct {
 	Configuration *Configuration `yaml:"configuration"`
 }
 
-func generatePassword(secretName string) error {
-	password := uniuri.NewLen(64)
+// generatePassword generates a password for `secretName` if it doesn't already exist
+func generatePassword(secretData map[string][]byte, secretName string) bool {
+	secretKey := strings.Replace(strings.ToLower(secretName), "_", "-", -1)
 
-	fmt.Println(secretName, password)
+	// Only create keys, don't update them
+	if _, ok := secretData[secretKey]; !ok {
+		password := uniuri.NewLen(64)
+		secretData[secretKey] = []byte(password)
+		return true
+	}
 
-	return nil
+	return false
+}
+
+func printHelp() {
+	fmt.Printf("Usage: %s <role-manifest>\n", os.Args[0])
 }
 
 func main() {
-	manifestFile, err := ioutil.ReadFile("role-manifest.yml")
+	if len(os.Args) != 2 {
+		printHelp()
+		os.Exit(1)
+	}
+
+	// Read the manifest file
+	manifestFile, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -105,16 +126,64 @@ func main() {
 		log.Fatal("'configuration section' not found in manifest")
 	}
 
+	// Set up access to the kube API
+	kubeConfig, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientSet, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	s := clientSet.CoreV1().Secrets(os.Getenv("KUBERNETES_NAMESPACE"))
+
+	// check for existing secret, initialize a new Secret if not found
+	create := false
+	secrets, err := s.Get(SECRET_NAME, metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Println("`secret` not found, creating")
+			create = true
+
+			secrets = &v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: SECRET_NAME,
+				},
+				Data: map[string][]byte{},
+			}
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	dirty := false
+
+	// go over the list of variables and run the appropriate generator function
 	for _, configVar := range manifest.Configuration.Variables {
 		if configVar.Secret && configVar.Generator != nil {
 			if configVar.Generator.Type == GeneratorTypePassword {
-				if err = generatePassword(configVar.Name); err != nil {
-					log.Fatal(err)
-				}
+				dirty = generatePassword(secrets.Data, configVar.Name) || dirty
 			} else if configVar.Generator.Type == GeneratorTypeCACertificate {
 			} else if configVar.Generator.Type == GeneratorTypeCertificate {
 			} else if configVar.Generator.Type == GeneratorTypeSSH {
 			}
 		}
+	}
+
+	// Create or update the secret
+	if create {
+		_, err = s.Create(secrets)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Created `secret`")
+	} else if dirty {
+		_, err = s.Update(secrets)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Updated `secret`")
 	}
 }
