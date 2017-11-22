a59ca17f6e1c758233fde83200bb81993f547d76
Alex Harford <alex.harford@suse.com>
2017-11-22 11:23:59 -0800

    Add password tests

---
+++ password/password_test.go
@@ -0,0 +1,32 @@
+package password
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewPasswordIsCreated(t *testing.T) {
+	assert := assert.New(t)
+
+	secretData := make(map[string][]byte)
+
+	result := GeneratePassword(secretData, "foo")
+
+	assert.True(result)
+	assert.Equal(len(secretData["foo"]), 64)
+}
+
+func TestExistingPasswordIsNotChanged(t *testing.T) {
+	assert := assert.New(t)
+
+	data := []byte("bar")
+
+	secretData := make(map[string][]byte)
+
+	secretData["foo"] = data
+
+	result := GeneratePassword(secretData, "foo")
+	assert.False(result)
+	assert.Equal(secretData["foo"], data)
+}
