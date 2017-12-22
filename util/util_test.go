cc82515a06371e984a4a198c31b1e9bd275ec285
Alex Harford <alex.harford@suse.com>
2017-12-22 14:48:30 -0800

    Add parallel flag to util tests

--- util/util_test.go
+++ util/util_test.go
@@ -17,6 +17,8 @@ func (m *MockLog) Fatal(str string, message ...interface{}) {
 }
 
 func TestConvertNameToKey(t *testing.T) {
+	t.Parallel()
+
 	assert := assert.New(t)
 
 	input := "APP_PASSPHRASE"
@@ -27,6 +29,8 @@ func TestConvertNameToKey(t *testing.T) {
 }
 
 func TestSetupEnv(t *testing.T) {
+	t.Parallel()
+
 	assert := assert.New(t)
 
 	osEnviron = func() []string {
@@ -41,28 +45,35 @@ func TestSetupEnv(t *testing.T) {
 func TestExpandEnvTemplates(t *testing.T) {
 	assert := assert.New(t)
 
-	origLogFatalf := logFatalf
-	defer func() {
-		logFatalf = origLogFatalf
-	}()
-
-	var mockLog MockLog
-	logFatalf = mockLog.Fatal
-
 	//
 	// Test that variable replacement works
 	//
-	env["FOO"] = "BAR"
-	assert.Equal(ExpandEnvTemplates("a {{.FOO}} variable"), "a BAR variable")
+	t.Run("VariableReplacementShouldWork", func(t *testing.T) {
+		t.Parallel()
+		env["FOO"] = "BAR"
+		assert.Equal(ExpandEnvTemplates("a {{.FOO}} variable"), "a BAR variable")
+	})
 
 	//
 	// Test that malformed templates fail
 	//
-	mockLog.On("Fatal",
-		"Can't parse templates in '%s': %s",
-		[]interface{}{"{{.bad", errors.New("template: :1: unclosed action")})
-	ExpandEnvTemplates("{{.bad")
-	mockLog.AssertCalled(t, "Fatal",
-		"Can't parse templates in '%s': %s",
-		[]interface{}{"{{.bad", errors.New("template: :1: unclosed action")})
+	t.Run("MalformedTemplatesShouldFail", func(t *testing.T) {
+		t.Parallel()
+
+		origLogFatalf := logFatalf
+		defer func() {
+			logFatalf = origLogFatalf
+		}()
+
+		var mockLog MockLog
+		logFatalf = mockLog.Fatal
+
+		mockLog.On("Fatal",
+			"Can't parse templates in '%s': %s",
+			[]interface{}{"{{.bad", errors.New("template: :1: unclosed action")})
+		ExpandEnvTemplates("{{.bad")
+		mockLog.AssertCalled(t, "Fatal",
+			"Can't parse templates in '%s': %s",
+			[]interface{}{"{{.bad", errors.New("template: :1: unclosed action")})
+	})
 }
