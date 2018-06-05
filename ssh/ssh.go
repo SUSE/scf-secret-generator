3c28e7eba601cc69934dc94a47c20ecb70cde453
Jan Dubois <jan.dubois@suse.com>
2018-06-05 16:07:43 -0700

    Reorder SSH generator and tests
    Make the source layout similar to the SSL code; make the tests sequence
    match the sequence in the source file.

--- ssh/ssh.go
+++ ssh/ssh.go
@@ -21,6 +21,38 @@ type Key struct {
 	Fingerprint string // Name to associate with fingerprint
 }
 
+// RecordKeyInfo records priave key or fingerprint names for later generation
+func RecordKeyInfo(keys map[string]Key, configVar *model.ConfigurationVariable) error {
+	if len(configVar.Generator.ID) == 0 {
+		return fmt.Errorf("Config variable `%s` has no ID value", configVar.Name)
+	}
+	if configVar.Generator.Type != model.GeneratorTypeSSH {
+		return fmt.Errorf("Config variable `%s` does not have a valid SSH generator type", configVar.Name)
+	}
+
+	// Get or create the key from the map, there should always be
+	// a pair of private keys and fingerprints
+	key := keys[configVar.Generator.ID]
+
+	switch configVar.Generator.ValueType {
+	case model.ValueTypeFingerprint:
+		if len(key.Fingerprint) > 0 {
+			return fmt.Errorf("Multiple variables define fingerprints name for SSH id `%s`", configVar.Generator.ID)
+		}
+		key.Fingerprint = configVar.Name
+	case model.ValueTypePrivateKey:
+		if len(key.PrivateKey) > 0 {
+			return fmt.Errorf("Multiple variables define private key name for SSH id `%s`", configVar.Generator.ID)
+		}
+		key.PrivateKey = configVar.Name
+	default:
+		return fmt.Errorf("Config variable `%s` has invalid value type `%s`", configVar.Name, configVar.Generator.ValueType)
+	}
+
+	keys[configVar.Generator.ID] = key
+	return nil
+}
+
 // GenerateAllKeys will create private keys and fingerprints for all recorded SSH variables
 func GenerateAllKeys(keys map[string]Key, secrets *v1.Secret) error {
 	for id, key := range keys {
@@ -72,37 +104,5 @@ func generateKey(secrets *v1.Secret, key Key) error {
 	secrets.Data[secretKey] = pem.EncodeToMemory(privateBlock)
 	secrets.Data[fingerprintKey] = []byte(ssh.FingerprintLegacyMD5(public))
 
-	return nil
-}
-
-// RecordKeyInfo records priave key or fingerprint names for later generation
-func RecordKeyInfo(keys map[string]Key, configVar *model.ConfigurationVariable) error {
-	if len(configVar.Generator.ID) == 0 {
-		return fmt.Errorf("Config variable `%s` has no ID value", configVar.Name)
-	}
-	if configVar.Generator.Type != model.GeneratorTypeSSH {
-		return fmt.Errorf("Config variable `%s` does not have a valid SSH generator type", configVar.Name)
-	}
-
-	// Get or create the key from the map, there should always be
-	// a pair of private keys and fingerprints
-	key := keys[configVar.Generator.ID]
-
-	switch configVar.Generator.ValueType {
-	case model.ValueTypeFingerprint:
-		if len(key.Fingerprint) > 0 {
-			return fmt.Errorf("Multiple variables define fingerprints name for SSH id `%s`", configVar.Generator.ID)
-		}
-		key.Fingerprint = configVar.Name
-	case model.ValueTypePrivateKey:
-		if len(key.PrivateKey) > 0 {
-			return fmt.Errorf("Multiple variables define private key name for SSH id `%s`", configVar.Generator.ID)
-		}
-		key.PrivateKey = configVar.Name
-	default:
-		return fmt.Errorf("Config variable `%s` has invalid value type `%s`", configVar.Name, configVar.Generator.ValueType)
-	}
-
-	keys[configVar.Generator.ID] = key
 	return nil
 }
--- ssh/ssh_test.go
+++ ssh/ssh_test.go
@@ -9,115 +9,6 @@ import (
 	"k8s.io/api/core/v1"
 )
 
-func TestGenerateKey(t *testing.T) {
-	t.Parallel()
-
-	t.Run("New key is created", func(t *testing.T) {
-
-		t.Parallel()
-
-		secrets := &v1.Secret{Data: map[string][]byte{}}
-
-		key := Key{
-			PrivateKey:  "foo",
-			Fingerprint: "bar",
-		}
-
-		err := generateKey(secrets, key)
-
-		require.NoError(t, err)
-		assert.Contains(t, string(secrets.Data["foo"]), "BEGIN RSA PRIVATE KEY")
-		assert.Contains(t, string(secrets.Data["foo"]), "END RSA PRIVATE KEY")
-
-		// 16 colon separated bytes = 47
-		// 00:11:22:33:44:55:66:77:88:99:aa:bb:cc:dd:ee:ff
-		assert.Len(t, secrets.Data["bar"], 47)
-	})
-
-	t.Run("Existing key is not changed", func(t *testing.T) {
-		t.Parallel()
-
-		fooData := []byte("foo-data")
-		barData := []byte("bar-data")
-
-		secrets := &v1.Secret{Data: map[string][]byte{}}
-
-		// Also tests for FOO / foo case conversion
-		secrets.Data["foo"] = fooData
-		secrets.Data["bar"] = barData
-
-		key := Key{
-			PrivateKey:  "FOO",
-			Fingerprint: "BAR",
-		}
-
-		err := generateKey(secrets, key)
-
-		require.NoError(t, err)
-		assert.Equal(t, fooData, secrets.Data["foo"])
-		assert.Equal(t, barData, secrets.Data["bar"])
-	})
-}
-
-func TestGenerateAllKeys(t *testing.T) {
-	t.Parallel()
-
-	t.Run("Generate keys", func(t *testing.T) {
-		t.Parallel()
-
-		secrets := &v1.Secret{Data: map[string][]byte{}}
-
-		keys := map[string]Key{
-			"mysshkey": Key{
-				PrivateKey:  "foo",
-				Fingerprint: "bar",
-			},
-		}
-
-		err := GenerateAllKeys(keys, secrets)
-
-		require.NoError(t, err)
-		assert.Contains(t, string(secrets.Data["foo"]), "BEGIN RSA PRIVATE KEY")
-		assert.Contains(t, string(secrets.Data["foo"]), "END RSA PRIVATE KEY")
-
-		// 16 colon separated bytes = 47
-		// 00:11:22:33:44:55:66:77:88:99:aa:bb:cc:dd:ee:ff
-		assert.Len(t, secrets.Data["bar"], 47)
-	})
-
-	t.Run("Missing private key", func(t *testing.T) {
-		t.Parallel()
-
-		secrets := &v1.Secret{Data: map[string][]byte{}}
-
-		keys := map[string]Key{
-			"mysshkey": Key{
-				Fingerprint: "bar",
-			},
-		}
-
-		err := GenerateAllKeys(keys, secrets)
-
-		assert.EqualError(t, err, "No private key name defined for SSH id `mysshkey`")
-	})
-
-	t.Run("Missing fingerprint", func(t *testing.T) {
-		t.Parallel()
-
-		secrets := &v1.Secret{Data: map[string][]byte{}}
-
-		keys := map[string]Key{
-			"mysshkey": Key{
-				PrivateKey: "foo",
-			},
-		}
-
-		err := GenerateAllKeys(keys, secrets)
-
-		assert.EqualError(t, err, "No fingerprint name defined for SSH id `mysshkey`")
-	})
-}
-
 func TestRecordKeyInfo(t *testing.T) {
 	t.Parallel()
 
@@ -293,5 +184,114 @@ func TestRecordKeyInfo(t *testing.T) {
 		err = RecordKeyInfo(keys, configVar)
 
 		assert.EqualError(t, err, "Multiple variables define private key name for SSH id `foo`")
+	})
+}
+
+func TestGenerateAllKeys(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Generate keys", func(t *testing.T) {
+		t.Parallel()
+
+		secrets := &v1.Secret{Data: map[string][]byte{}}
+
+		keys := map[string]Key{
+			"mysshkey": Key{
+				PrivateKey:  "foo",
+				Fingerprint: "bar",
+			},
+		}
+
+		err := GenerateAllKeys(keys, secrets)
+
+		require.NoError(t, err)
+		assert.Contains(t, string(secrets.Data["foo"]), "BEGIN RSA PRIVATE KEY")
+		assert.Contains(t, string(secrets.Data["foo"]), "END RSA PRIVATE KEY")
+
+		// 16 colon separated bytes = 47
+		// 00:11:22:33:44:55:66:77:88:99:aa:bb:cc:dd:ee:ff
+		assert.Len(t, secrets.Data["bar"], 47)
+	})
+
+	t.Run("Missing private key", func(t *testing.T) {
+		t.Parallel()
+
+		secrets := &v1.Secret{Data: map[string][]byte{}}
+
+		keys := map[string]Key{
+			"mysshkey": Key{
+				Fingerprint: "bar",
+			},
+		}
+
+		err := GenerateAllKeys(keys, secrets)
+
+		assert.EqualError(t, err, "No private key name defined for SSH id `mysshkey`")
+	})
+
+	t.Run("Missing fingerprint", func(t *testing.T) {
+		t.Parallel()
+
+		secrets := &v1.Secret{Data: map[string][]byte{}}
+
+		keys := map[string]Key{
+			"mysshkey": Key{
+				PrivateKey: "foo",
+			},
+		}
+
+		err := GenerateAllKeys(keys, secrets)
+
+		assert.EqualError(t, err, "No fingerprint name defined for SSH id `mysshkey`")
+	})
+}
+
+func TestGenerateKey(t *testing.T) {
+	t.Parallel()
+
+	t.Run("New key is created", func(t *testing.T) {
+
+		t.Parallel()
+
+		secrets := &v1.Secret{Data: map[string][]byte{}}
+
+		key := Key{
+			PrivateKey:  "foo",
+			Fingerprint: "bar",
+		}
+
+		err := generateKey(secrets, key)
+
+		require.NoError(t, err)
+		assert.Contains(t, string(secrets.Data["foo"]), "BEGIN RSA PRIVATE KEY")
+		assert.Contains(t, string(secrets.Data["foo"]), "END RSA PRIVATE KEY")
+
+		// 16 colon separated bytes = 47
+		// 00:11:22:33:44:55:66:77:88:99:aa:bb:cc:dd:ee:ff
+		assert.Len(t, secrets.Data["bar"], 47)
+	})
+
+	t.Run("Existing key is not changed", func(t *testing.T) {
+		t.Parallel()
+
+		fooData := []byte("foo-data")
+		barData := []byte("bar-data")
+
+		secrets := &v1.Secret{Data: map[string][]byte{}}
+
+		// Also tests for FOO / foo case conversion
+		secrets.Data["foo"] = fooData
+		secrets.Data["bar"] = barData
+
+		key := Key{
+			PrivateKey:  "FOO",
+			Fingerprint: "BAR",
+		}
+
+		err := generateKey(secrets, key)
+
+		require.NoError(t, err)
+		assert.Equal(t, fooData, secrets.Data["foo"])
+		assert.Equal(t, barData, secrets.Data["bar"])
 	})
 }
